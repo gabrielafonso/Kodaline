2f44bcc95bff91f5ec7d280e64fcd3ba3404e070
Janaine <janainesantosifal@gmail.com>
2018-10-23 09:10:49 -0300

    Adição do Maven na pasta Kodaline

---
+++ src/test/java/com/afonjasam/Kodaline/KodalineApplicationTests.java
@@ -0,0 +1,16 @@
+package com.afonjasam.Kodaline;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class KodalineApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
