b8fb442b731aad9ee13b4d9f3a57ca1e1905b598
Afonso <gabrielafonsojustino@gmail.com>
2018-10-23 11:23:45 -0300

    Documentção e Mapeamento das classes

--- src/main/java/kodaline/model/Pessoa.java
+++ src/main/java/kodaline/model/Pessoa.java
@@ -13,7 +13,7 @@ public class Pessoa{
 	private Long id;
 	@Column(nullable=false)
 	private String nome;
-	@Column(nullable=false)
+	@Column(nullable=false, unique=true)
 	private String email;
 	@Column(nullable=false)
 	private String telefone;
