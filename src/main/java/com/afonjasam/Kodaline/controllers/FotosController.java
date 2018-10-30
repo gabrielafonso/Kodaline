bf1aee09cb5b3bfdb1880af6e962c75407aa3864
Janaine Santos <janainesantosifal@gmail.com>
2018-10-30 11:31:28 -0300

    Implementação da classe fotoscontroller

--- src/main/java/com/afonjasam/Kodaline/controllers/FotosController.java
+++ src/main/java/com/afonjasam/Kodaline/controllers/FotosController.java
@@ -1,5 +1,26 @@
 package com.afonjasam.Kodaline.controllers;
 
+import java.awt.print.Pageable;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.afonjasam.Kodaline.model.Fotos;
+import com.afonjasam.Kodaline.repository.FotosRepository;
+
+@RestController
 public class FotosController {
+	
+	@Autowired
+	private FotosRepository fotosrepository;
+	
+	@GetMapping("/fotos")
+	public Page<Fotos> getFoto(Pageable pageable){
+		return fotosrepository.findAll(pageable);
+		
+	}
+	
 
 }
