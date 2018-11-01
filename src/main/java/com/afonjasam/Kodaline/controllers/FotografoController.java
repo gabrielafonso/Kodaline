a8f9dd8d8d936e61ab64615c2dc9d13d716c7607
Luiz Fernando <luiz18659@gmail.com>
2018-11-01 12:19:11 -0300

    Implementação na classe FotografoController

--- src/main/java/com/afonjasam/Kodaline/controllers/FotografoController.java
+++ src/main/java/com/afonjasam/Kodaline/controllers/FotografoController.java
@@ -1,5 +1,40 @@
 package com.afonjasam.Kodaline.controllers;
 
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.afonjasam.Kodaline.orElseThrowmodel.Fotografo;
+import com.afonjasam.Kodaline.repository.FotografoRepository;
+
+@RestController
 public class FotografoController {
 
+	@Autowired
+	private FotografoRepository fotograforepository;
+	
+	@GetMapping("/fotografo")
+	public Page<Fotografo>getFotografo(Pageable pageable){
+		return fotograforepository.findAll(pageable);
+	}
+	
+	@PostMapping("/fotografo")
+	public Fotografo createFotografo(@Valid @RequestBody Fotografo fotografo){
+		return fotograforepository.save(fotografo);
+}
+	@PostMapping("/fotografo /{fotografoId}")
+	public Fotografo updateFotografo (@PathVariable Long fotografoId, @Valid @RequestBody Fotografo fotografoRequest) {
+		return fotograforepository.findById(fotografoId).map(fotografo->{fotografo.setAlbum(fotografoRequest.getFotos());
+		return fotograforepository.save(fotografo);
+		}).orElseThrow(()-> new ResourceNotFoundException("Fotografo não encontrado:"+fotografoId));
+	}
+	
+	@DeleteMapping()
 }
