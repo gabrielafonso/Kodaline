3387bfe03563d390dc0a3f4efbb6373a4fb10c51
“Janaine <janainesantosifal@gmail.com>
2018-11-01 12:07:50 -0300

    Crição do pacote exception e implementação do controller na classe Fotos

--- src/main/java/com/afonjasam/Kodaline/controllers/FotosController.java
+++ src/main/java/com/afonjasam/Kodaline/controllers/FotosController.java
@@ -1,26 +1,57 @@
 package com.afonjasam.Kodaline.controllers;
 
-import java.awt.print.Pageable;
+import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-
+import com.afonjasam.Kodaline.exception.ResourceNotFoundException;
 import com.afonjasam.Kodaline.model.Fotos;
 import com.afonjasam.Kodaline.repository.FotosRepository;
+
 
 @RestController
 public class FotosController {
 	
 	@Autowired
-	private FotosRepository fotosrepository;
+	private FotosRepository fotosRepository;
 	
 	@GetMapping("/fotos")
 	public Page<Fotos> getFoto(Pageable pageable){
-		return fotosrepository.findAll(pageable);
-		
+		return fotosRepository.findAll(pageable);	
 	}
 	
+	@PostMapping("/familymember")
+	public Fotos creatFotos(@Valid @RequestBody Fotos fotos) {
+		return fotosRepository.save(fotos);	
+	}
+	
+	@PutMapping("/fotos/{fotosId}")
+	public Fotos updateFotos (@PathVariable Long fotosId,
+			@Valid @RequestBody Fotos fotosRequest) {
+            	return fotosRepository.findById(fotosId)
+				.map(fotos -> {
+					fotos.setDate(fotosRequest.getDate());
+					fotos.setDetalhes(fotosRequest.getDetalhes());
+					return fotosRepository.save(fotos);
+		}).orElseThrow(() -> new ResourceNotFoundException("Fotos not found: " + fotosId));
+	}
+	
+	@DeleteMapping("/fotos/{fotosId}")
+	public ResponseEntity<?> deleteQuestion(@PathVariable Long fotosId){
+		return fotosRepository.findById(fotosId)
+				.map(fotos -> {
+					fotosRepository.delete(fotos);
+					return ResponseEntity.ok().build();
+				}).orElseThrow(() -> new ResourceNotFoundException("Fotos not found: " + fotosId));
+	}
 
 }
