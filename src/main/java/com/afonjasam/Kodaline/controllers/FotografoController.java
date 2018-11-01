2800f97f66e52037c6e2ab9951fdcc02cf9f18ac
“Janaine <janainesantosifal@gmail.com>
2018-11-01 17:28:18 -0300

    Correção na classe FotografoController

--- src/main/java/com/afonjasam/Kodaline/controllers/FotografoController.java
+++ src/main/java/com/afonjasam/Kodaline/controllers/FotografoController.java
@@ -5,14 +5,18 @@ import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.afonjasam.Kodaline.orElseThrowmodel.Fotografo;
+import com.afonjasam.Kodaline.exception.ResourceNotFoundException;
 import com.afonjasam.Kodaline.repository.FotografoRepository;
+import com.afonjasam.Kodaline.model.Fotografo;
 
 @RestController
 public class FotografoController {
@@ -21,7 +25,7 @@ public class FotografoController {
 	private FotografoRepository fotograforepository;
 	
 	@GetMapping("/fotografo")
-	public Page<Fotografo>getFotografo(Pageable pageable){
+	public Page<Fotografo>getFotografos(Pageable pageable){
 		return fotograforepository.findAll(pageable);
 	}
 	
@@ -29,12 +33,24 @@ public class FotografoController {
 	public Fotografo createFotografo(@Valid @RequestBody Fotografo fotografo){
 		return fotograforepository.save(fotografo);
 }
-	@PostMapping("/fotografo /{fotografoId}")
-	public Fotografo updateFotografo (@PathVariable Long fotografoId, @Valid @RequestBody Fotografo fotografoRequest) {
-		return fotograforepository.findById(fotografoId).map(fotografo->{fotografo.setAlbum(fotografoRequest.getFotos());
-		return fotograforepository.save(fotografo);
-		}).orElseThrow(()-> new ResourceNotFoundException("Fotografo não encontrado:"+fotografoId));
+	@PutMapping("/fotografo /{fotografoId}")
+	public Fotografo updateFotografo (@PathVariable Long fotografoId, 
+			                          @Valid @RequestBody Fotografo fotografoRequest) {
+		return fotograforepository.findById(fotografoId)
+				.map(fotografo->{
+					fotografo.setAlbum(fotografoRequest.getAlbum());
+					fotografo.setFotos(fotografoRequest.getFotos());
+		            return fotograforepository.save(fotografo);
+		}).orElseThrow(()-> new ResourceNotFoundException("Fotografo not found: " +fotografoId));
 	}
 	
-	@DeleteMapping()
+	@DeleteMapping("/fotografo/{fotografoId}")
+	public ResponseEntity<?> deleteQuestion(@PathVariable Long fotografoId){
+		return fotograforepository.findById(fotografoId)
+				.map(fotografo -> {
+					fotograforepository.delete(fotografo);
+					return ResponseEntity.ok().build();
+				}).orElseThrow(() -> new ResourceNotFoundException("Fotografo not found: " + fotografoId));
+	}
+	
 }
