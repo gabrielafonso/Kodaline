e9e1fbabec2696524ad3a3a313ac676143031598
Samuel Veloso <samuelveloso42@gmail.com>
2018-11-23 09:09:28 -0300

    Adição de atributos e metodos da classe Usuario

--- src/main/java/com/afonjasam/Kodaline/model/Usuario.java
+++ src/main/java/com/afonjasam/Kodaline/model/Usuario.java
@@ -1,5 +1,27 @@
 package com.afonjasam.Kodaline.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+
+@Entity
 public class Usuario {
 
+	@Id	
+	@GeneratedValue
+	private Long id;
+	
+	@Column(name = "email", nullable=false, unique=true)
+	private String email;
+	@Column(name = "password", nullable=false, unique=true)
+	private String password;
+	
+	@OneToMany(
+			mappedBy = "owner",
+			cascade = CascadeType.ALL,
+			orphanRemoval = true
+	)
+	private List<Telefone> phones = new ArrayList<>();
 }
