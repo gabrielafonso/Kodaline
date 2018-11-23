4b906df64f45a33b52c4f3506cd9410021f78ef4
Samuel Veloso <samuelveloso42@gmail.com>
2018-11-23 09:17:48 -0300

    Remoção de argumentos na classe Cliente

--- src/main/java/com/afonjasam/Kodaline/model/Cliente.java
+++ src/main/java/com/afonjasam/Kodaline/model/Cliente.java
@@ -1,13 +1,12 @@
 package com.afonjasam.Kodaline.model;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import javax.persistence.*;
-import javax.validation.constraints.NotBlank;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 
 @Entity
-public class Cliente {
+public class Cliente extends Usuario {
 
 	@Id	
 	@GeneratedValue
@@ -15,10 +14,6 @@ public class Cliente {
 	
 	@Column(name = "nome" ,nullable=false)
 	private String nome;
-	@Column(name = "email", nullable=false, unique=true)
-	private String email;
-	@Column(name = "telefone", nullable=false)
-	private String telefone;
 	@Column(name = "dados_bancarios",nullable=false)
 	private String dadosBancarios;
 	
@@ -34,37 +29,24 @@ public class Cliente {
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
-	public String getEmail() {
-		return email;
-	}
-	public void setEmail(String email) {
-		this.email = email;
-	}
-	public String getTelefone() {
-		return telefone;
-	}
-	public void setTelefone(String telefone) {
-		this.telefone = telefone;
-	}
+
 	public Long getId() {
 		return id;
 	}
 	@Override
 	public int hashCode() {
 		final int prime = 31;
-		int result = 1;
+		int result = super.hashCode();
 		result = prime * result + ((dadosBancarios == null) ? 0 : dadosBancarios.hashCode());
-		result = prime * result + ((email == null) ? 0 : email.hashCode());
 		result = prime * result + ((id == null) ? 0 : id.hashCode());
 		result = prime * result + ((nome == null) ? 0 : nome.hashCode());
-		result = prime * result + ((telefone == null) ? 0 : telefone.hashCode());
 		return result;
 	}
 	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
 			return true;
-		if (obj == null)
+		if (!super.equals(obj))
 			return false;
 		if (getClass() != obj.getClass())
 			return false;
@@ -73,11 +55,6 @@ public class Cliente {
 			if (other.dadosBancarios != null)
 				return false;
 		} else if (!dadosBancarios.equals(other.dadosBancarios))
-			return false;
-		if (email == null) {
-			if (other.email != null)
-				return false;
-		} else if (!email.equals(other.email))
 			return false;
 		if (id == null) {
 			if (other.id != null)
@@ -89,14 +66,7 @@ public class Cliente {
 				return false;
 		} else if (!nome.equals(other.nome))
 			return false;
-		if (telefone == null) {
-			if (other.telefone != null)
-				return false;
-		} else if (!telefone.equals(other.telefone))
-			return false;
 		return true;
 	}
-
-	
 
 }
