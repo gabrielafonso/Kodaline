c454a53dbf4478e358cf5884dbe955cd84eb21ce
SamuelV3loso <samuelveloso42@gmail.com>
2018-12-02 02:58:50 -0300

    ned
    Criação da classe UsuarioTesteValidacao.java e seus metodos

--- src/main/java/com/afonjasam/Kodaline/servicos/UsuarioServico.java
+++ src/main/java/com/afonjasam/Kodaline/servicos/UsuarioServico.java
@@ -1,5 +1,22 @@
 package com.afonjasam.Kodaline.servicos;
 
-public class ServicoValidacao {
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class UsuarioServico {
+
+	public boolean validarUsuario(String email, String password) {
+		
+        if (email != null && email.length() > 0 && password.length() >= 8) {
+            String expression = "^[\\w\\.-]+@([\\w\\-]+\\.)+[A-Z]{2,4}$";
+            Pattern pattern = Pattern.compile(expression, Pattern.CASE_INSENSITIVE);
+            Matcher matcher = pattern.matcher(email);
+            if (matcher.matches()) {
+                return true;
+            }
+        }
+        return false;
+		
+	}
 	
 }
