ff61c99fa4652299db74f8856fe78a3355c1384c
SamuelV3loso <samuelveloso42@gmail.com>
2018-12-02 23:12:46 -0300

    Adição de metodo de validação na classe TelefoneServico

--- src/main/java/com/afonjasam/Kodaline/servicos/TelefoneServico.java
+++ src/main/java/com/afonjasam/Kodaline/servicos/TelefoneServico.java
@@ -2,5 +2,16 @@ package com.afonjasam.Kodaline.servicos;
 
 public class TelefoneServico {
 
-	
+	public boolean validarTelefone(String telefone) {
+		
+		if(telefone.length() == 11) {
+			for(int i = 0; i < telefone.length() ;i++) {
+				if(Character.isDigit(telefone.charAt(i))) {
+					return true;
+				}else return false;
+			}
+		}
+		
+		return false;
+	}
 }
