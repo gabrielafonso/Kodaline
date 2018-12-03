c7afa3ce7e4c5d62b63bbe4fe3a808e18554f599
SamuelV3loso <samuelveloso42@gmail.com>
2018-12-02 23:56:00 -0300

    Criação da classe FotoTesteValidacao

---
+++ src/test/java/com/afonjasam/Kodaline/FotoTesteValidacao.java
@@ -0,0 +1,13 @@
+package com.afonjasam.Kodaline;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import com.afonjasam.Kodaline.servicos.FotosServico;
+
+public class FotoTesteValidacao {
+
+	
+}
