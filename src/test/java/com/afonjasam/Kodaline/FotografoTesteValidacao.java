05f592eb12f5971c556392e3221f3e6cabf134fc
SamuelV3loso <samuelveloso42@gmail.com>
2018-12-02 22:43:02 -0300

    Implementação de testes na classe FotografoTesteValidacao

--- src/test/java/com/afonjasam/Kodaline/FotografoTesteValidacao.java
+++ src/test/java/com/afonjasam/Kodaline/FotografoTesteValidacao.java
@@ -1,5 +1,41 @@
 package com.afonjasam.Kodaline;
 
+
+import static org.junit.Assert.*;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import com.afonjasam.Kodaline.servicos.FotografoServico;
+
 public class FotografoTesteValidacao {
+
+	FotografoServico ft;
+	
+	@Before
+	public void setUp() throws Exception {
+		ft = new FotografoServico();
+	}
+
+	@Test
+	public void testValidarFotografoCorreto() {
+		Assert.assertEquals(true, ft.validarFotografo("João", "Silva"));
+	}
+	
+	@Test
+	public void testValidarFotografoMinusculo() {
+		Assert.assertEquals(false, ft.validarFotografo("joão", "silva"));
+	}
+	
+	@Test
+	public void testValidarFotografoEmBranco() {
+		Assert.assertEquals(false, ft.validarFotografo("", ""));
+	}
+	
+	@Test
+	public void testValidarFotografoComEspaco() {
+		Assert.assertEquals(false, ft.validarFotografo(" ", " "));
+	}
 
 }
