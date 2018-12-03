3dbe0b2fe2cd537f7551589e74fc7abc761e87eb
SamuelV3loso <samuelveloso42@gmail.com>
2018-12-02 23:53:15 -0300

    Adição de metodo de validação para a classe FotosServico

--- src/main/java/com/afonjasam/Kodaline/servicos/FotosServico.java
+++ src/main/java/com/afonjasam/Kodaline/servicos/FotosServico.java
@@ -1,5 +1,16 @@
 package com.afonjasam.Kodaline.servicos;
 
+import java.time.LocalDate;
+
 public class FotosServico {
 
+	public boolean validarFoto(String detalhes) {
+		
+		if(detalhes.length() > 10) {
+			return true;
+		}
+		
+		return false;
+	}
+	
 }
