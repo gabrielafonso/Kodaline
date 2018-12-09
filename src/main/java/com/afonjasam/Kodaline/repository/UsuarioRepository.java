d4bb81cdf005456f47739cdf38e10f2e9e954913
SamuelV3loso <samuelveloso42@gmail.com>
2018-12-09 01:58:47 -0300

    Criação da classe UsuarioRepository.java e seus metodos

---
+++ src/main/java/com/afonjasam/Kodaline/repository/UsuarioRepository.java
@@ -0,0 +1,11 @@
+package com.afonjasam.Kodaline.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.afonjasam.Kodaline.model.Usuario;
+
+@Repository
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+
+}
