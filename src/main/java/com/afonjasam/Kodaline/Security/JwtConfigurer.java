9985460c872aae7ef936640082b9fe3b8a439b9a
kelly <kellbianca0@gmail.com>
2018-12-18 10:02:34 -0300

    Correcao na classe JwtConfigurer

--- src/main/java/com/afonjasam/Kodaline/Security/JwtConfigurer.java
+++ src/main/java/com/afonjasam/Kodaline/Security/JwtConfigurer.java
@@ -4,7 +4,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.DefaultSecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
-public class JwtConfigurer extends private JwtTokenProvider jwtTokenProvider;
+public class JwtConfigurer extends SecurityConfigurerAdapter<DefaultSecurityFilterChain, HttpSecurity>{
 
 private JwtTokenProvider jwtTokenProvider;
 
